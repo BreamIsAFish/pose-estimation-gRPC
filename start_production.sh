8a765197f3b8f8a85fff32eeb85c069edd70c235
Beam <beam7684@gmail.com>
2022-05-06 15:28:43 +0700

    separate server from app

--- start_production.sh
+++ start_production.sh
@@ -2,12 +2,12 @@
 
 # NODE_ENV=production node dist/apps/tuto-real-backend/main.js &
 # cd gRPC/
-echo '''Starting game server...'''
-python3 ./gRPC/gameServer.py &
+# echo '''Starting game server...'''
+# python3 ./gRPC/gameServer.py &
 
 # cd ../
-echo '''Starting pose server...'''
-node poseServer &
+# echo '''Starting pose server...'''
+# node poseServer &
 echo '''Starting pose app...''' 
 ./node_modules/.bin/pm2 serve ./ 4200 --spa  &
 
